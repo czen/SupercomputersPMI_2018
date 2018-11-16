0e4bdb14583e963ab74d51e083c410b7fac577ef
Sirozu <43061026+Sirozu@users.noreply.github.com>
2018-11-16 10:48:49 +0300

    Galicky_Ind_Task_Var_28

---
+++ Galicky_Int_Task_Var_28/matrix.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifndef MATR
+#define MATR
+
+#define SIZE 100
+#define BLOCK_SIZE 10
+#define LENGTH (SIZE + BLOCK_SIZE)*SIZE/2
+
+#define FULL_BLOCK BLOCK_SIZE * BLOCK_SIZE
+#define FULL_LINE  BLOCK_SIZE * SIZE
+
+#endif
